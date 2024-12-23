b58429d89c0d8cf35556e6be05ae24ba73dd5e32
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2024-12-23 02:36:14 +0100

    Add logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 /**
  * @import {BuildOptions} from './modules/shared/types'
  * @import {InternalBuildOptions} from './modules/shared/types'
+ * @import {Logger} from 'pino'
  */
 
 import { Builder } from './modules/builder/builder.js';
 import { Bundler } from './modules/bundler/bundler.js';
 import { Cleaner } from './modules/cleaner/cleaner.js';
+import { pino } from 'pino';
+
+const DEFAULT_LOGGER = pino({
+  transport: {
+    target: 'pino-pretty'
+  }
+});
 
 const DEFAULT_VERSION = Math.floor(Date.now() / 1000).toString();
 
@@ -67,20 +75,28 @@ export const DEFAULT_BUILD_OPTIONS = {
 
 /**
  * @param {BuildOptions} options
+ * @param {Logger | null} logger
  * @returns {Promise<void>}
  */
-export async function build(options) {
+export async function build(options, logger = DEFAULT_LOGGER) {
   const buildOptions = { ...DEFAULT_BUILD_OPTIONS, ...options };
 
   const builder = new Builder(buildOptions);
+  logger?.info('Emptying the ouput directory.');
   await builder.emptyOutDirectory();
+  logger?.info('Querying directories and files.');
   const [directories, files] = await builder.getDirectoriesAndFiles();
+  logger?.info('Creating the directory structure.');
   await builder.createDirectoryStructure(directories);
+  logger?.info('Copying files.');
   await builder.copyFiles(files);
 
   const bundler = new Bundler(buildOptions);
+  logger?.info('Querying entry points.');
   const entryPoints = await bundler.getEntryPoints(files);
+  logger?.info('Bundling.');
   await bundler.bundle(entryPoints);
 
+  logger?.info('Cleaning up.');
   await new Cleaner(buildOptions).cleanUp();
 }
